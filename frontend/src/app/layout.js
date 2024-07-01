6b42e6ec612c7d7b0fb2b2afc6e9b79068ad3cdf
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2024-07-01 21:16:48 +0900

    feat : frontend

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {/* Layout UI */}
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
