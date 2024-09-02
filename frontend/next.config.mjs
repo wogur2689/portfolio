fc76d06e1af6483a928608df42f1571a0befde1d
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2024-09-02 21:20:28 +0900

    fix : 빌드에러로 인한 common 주석처리

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    output: 'export',
+    trailingSlash: true, // 경로 뒤에 슬래시를 추가하여 파일 탐색 가능하게 설정
     images: {
-        unoptimized: true,
-        disableStaticImages: true
+        unoptimized: true  // 이미지 최적화 비활성화 (필요에 따라 설정)
     }
 };
 
--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,22 +1,20 @@
+"use client";
+
 import styles from './app.module.css';
 import Career from "./career/page";
 import Home from "./home/page";
 import Projects from "./projects/page";
 import Skills from "./skills/page";
-import {MetaHead, Header, Footer} from "./common/page";
 
 export default function RootLayout() {
   return (
     <html lang="ko">
-      <MetaHead></MetaHead>
       <body className={styles.body}>
         <main>
-          <Header></Header>
           <Home></Home>
           <Skills></Skills>
           <Projects></Projects>
           <Career></Career>
-          <Footer></Footer>
         </main>
       </body>
     </html>
