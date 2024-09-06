417397f12a357676ff17fcef45122fbcbafe6fbe
길재혁 <81075170+wogur2689@users.noreply.github.com>
2024-09-06 16:25:27 +0900

    fix : 설정 일부 주석처리

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,10 +3,10 @@ const nextConfig = {
     basePath: '/portfoilo',
     assetPrefix: '/portfoilo',
     output: 'export',
-    trailingSlash: true, // 경로 뒤에 슬래시를 추가하여 파일 탐색 가능하게 설정
-    images: {
-        unoptimized: true  // 이미지 최적화 비활성화 (필요에 따라 설정)
-    }
+    // trailingSlash: true, // 경로 뒤에 슬래시를 추가하여 파일 탐색 가능하게 설정
+    // images: {
+    //     unoptimized: true  // 이미지 최적화 비활성화 (필요에 따라 설정)
+    // }
 };
 
 export default nextConfig;
