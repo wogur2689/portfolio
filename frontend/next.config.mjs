511b69edc7510aad189a183d684e1a4c5f10a048
길재혁 <81075170+wogur2689@users.noreply.github.com>
2024-09-06 16:50:21 +0900

    fix : 경로 수정

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/portfolio/frontend',
-    assetPrefix: '/portfolio/frontend',
     output: 'export',
+    basePath: '/portfolio',
+    assetPrefix: '/portfolio'
     // trailingSlash: true, // 경로 뒤에 슬래시를 추가하여 파일 탐색 가능하게 설정
     // images: {
     //     unoptimized: true  // 이미지 최적화 비활성화 (필요에 따라 설정)
