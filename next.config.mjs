f134f28ca48e45b815ea5d2e561a3b50c5bd0666
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2024-09-07 13:43:53 +0900

    fix : 설정 변경

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
     output: 'export',
     basePath: '/portfolio',
-    assetPrefix: '/portfolio/',
-    // trailingSlash: true, // 경로 뒤에 슬래시를 추가하여 파일 탐색 가능하게 설정
+    assetPrefix: '/portfolio',
+    trailingSlash: true, // 경로 뒤에 슬래시를 추가하여 파일 탐색 가능하게 설정
     images: {
         unoptimized: true  // 이미지 최적화 비활성화 (필요에 따라 설정)
     }
