3663bda094d78f91b42aafce4ba21efdfbb13c41
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2024-10-03 23:46:21 +0900

    fix : project data 생성

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,5 +1,32 @@
 import styles from './projects.module.css';
 
+//project data
+const data = () => {
+    return [ 
+        {'id' : 1, 'title' : 'Music Web', 'outline' : '음악을 들을수 있는 사이트로 React를 확실히 공부하면서 프로젝트도 해보고 싶어서 만들어본 사이트'},
+        {'id' : 2, 'title' : 'Music Web', 'outline' : '음악을 들을수 있는 사이트로 React를 확실히 공부하면서 프로젝트도 해보고 싶어서 만들어본 사이트'},
+        {'id' : 3, 'title' : 'Music Web', 'outline' : '음악을 들을수 있는 사이트로 React를 확실히 공부하면서 프로젝트도 해보고 싶어서 만들어본 사이트'},
+    ]
+}
+
+function modalpopup() {
+    return <>
+        <div id="myModal" class="modal">
+            <div class="modal-content">
+                <span class="close" onclick="closeModal()">&times;</span>
+                    <h1>프로젝트 : <a href="https://wogur2689.github.io/MusicWeb/">Music Web</a></h1>
+                    <span>프로젝트 제목을 클릭시 사이트로 이동합니다.</span>
+                    <p>
+                        <h3>[개요]<br/>음악을 들을수 있는 사이트로 React를 확실히 공부하면서 프로젝트도 해보고 싶어서 만들어본 사이트</h3>
+                        <h3>[기술]<br/>React, Pigma</h3>
+                        <h3>[기간]<br/>2022.01 ~ 2022.06 6개월</h3>
+                    </p>
+                <br />
+            </div>
+        </div>
+    </>
+}
+
 export default function Projects() {
     return (
         <>
