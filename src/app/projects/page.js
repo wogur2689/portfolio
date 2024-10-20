7527aed07947b5569ab0271b3eaf03a1787dbba4
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2024-10-21 00:19:08 +0900

    fix : 데이터 skill, url, period 추가

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,14 +1,56 @@
 import styles from './projects.module.css';
 
-//project data
+//project data 추후 백엔드로 변경
 const data = () => {
     return [ 
-        {'id' : 1, 'title' : 'Music Web', 'outline' : '음악을 들을수 있는 사이트로 React를 확실히 공부하면서 프로젝트도 해보고 싶어서 만들어본 사이트'},
-        {'id' : 2, 'title' : 'plant app', 'outline' : '식물을 촬영해서 어떤 식물인지 알아내는 어플'},
-        {'id' : 3, 'title' : 'chatting', 'outline' : 'redis 사용법과 소켓 통신을 경험하고 싶어 만든 채팅'},
-        {'id' : 4, 'title' : 'DMC App', 'outline' : '동양미래대학교를 졸업한 선배로서 학교생활에 대한 정보를 제공하는 팀프로젝트 어플'},
-        {'id' : 5, 'title' : 'DMC Site', 'outline' : 'DMC App을 만든 팀으로 우리 팀을 소개하기 위해 개발된 팀프로젝트 사이트'},
-        {'id' : 6, 'title' : 'dadok dadok', 'outline' : '독서 완독을 장려하는 팀프로젝트 사이트'},
+        {
+            'id' : 1,
+            'title' : 'Music Web',
+            'outline' : '음악을 들을수 있는 사이트로 React를 확실히 공부하면서 프로젝트도 해보고 싶어서 만들어본 사이트',
+            'skill' : 'React, Figma',
+            'period': '2022.01 ~ 2022.06 (6개월)',
+            'url': 'https://wogur2689.github.io/MusicWeb'
+        },
+        {
+            'id' : 2, 
+            'title' : 'plant app',
+            'outline' : '식물을 촬영해서 어떤 식물인지 알아내는 어플',
+            'skill' : 'Android, kotlin, Figma',
+            'period': '2022.08 ~ 2022.10 (3개월)',
+            'url': 'https://github.com'
+        },
+        {
+            'id' : 3,
+            'title' : 'chatting',
+            'outline' : 'redis 사용법과 소켓 통신을 경험하고 싶어 만든 채팅',
+            'skill' : 'Spring boot, ws-stomp, JQuery, sockjs, thymeleaf, websocket, redis',
+            'period': '2023.09 ~ 2023.10 (2개월)',
+            'url': 'https://github.com'
+        },
+        {
+            'id' : 4,
+            'title' : 'DMC Site',
+            'outline' : 'DMC App을 만든 팀으로 우리 팀을 소개하기 위해 개발된 팀프로젝트 사이트',
+            'skill' : 'Spring boot, mysql, React, Pigma, github pages',
+            'period': '2022.10 ~ 2022.12 (3개월)',
+            'url': 'https://github.com'
+        },
+        {
+            'id' : 5,
+            'title' : 'DMC App',
+            'outline' : '동양미래대학교를 졸업한 선배로서 학교생활에 대한 정보를 제공하는 팀프로젝트 어플',
+            'skill' : 'Spring boot, firebase, React, Pigma, android, kotlin',
+            'period': '2023.01 ~ 2023.10 (9개월)',
+            'url': 'https://github.com'
+        },
+        {
+            'id' : 6,
+            'title' : 'dadok dadok',
+            'outline' : '독서 완독을 장려하는 팀프로젝트 사이트',
+            'skill' : 'Spring boot, mysql, swagger, JUnit, Pigma',
+            'period': '2024.02 ~ 2024.08 (7개월)',
+            'url': 'https://github.com'
+        },
     ]
 }
 
@@ -24,15 +66,15 @@ export const ModalPopup = (props) => {
             <div className="modal-content">
                 <span className="close" onClick={() => closeModal()}>&times;</span>
                 <h1>프로젝트: 
-                    <a href={`https://wogur2689.github.io/${projectData.title.replace(/\s+/g, '')}/`}>
+                    <a href={`${projectData.url}`}>
                         {projectData.title}
                     </a>
                 </h1>
                 <span>프로젝트 제목을 클릭 시 사이트로 이동합니다.</span>
                 <p>
                     <h3>[개요]<br/>{projectData.outline}</h3>
-                    <h3>[기술]<br/>React, Figma</h3>
-                    <h3>[기간]<br/>2022.01 ~ 2022.06 (6개월)</h3>
+                    <h3>[기술]<br/>{projectData.skill}</h3>
+                    <h3>[기간]<br/>{projectData.period}</h3>
                 </p>
             </div>
         </div>
@@ -51,23 +93,23 @@ export default function Projects() {
                     </div>
                     <div className={styles.project}>
                         <img src="./img/project/Plant.png" alt="식물사전앱"/>
-                        <span><a href="">Plant App</a></span>
+                        <span><a href="#"><button type="button" onclick="modalpopup(2)">Plant App</button></a></span>
                     </div>
                     <div className={styles.project}>
                         <img src="./img/project/Chatting.png" alt="채팅"/>
-                        <span><a href="">Mini Chatting</a></span>
+                        <span><a href="#"><button type="button" onclick="modalpopup(3)">Mini Chatting</button></a></span>
                     </div>
                     <div className={styles.project}>
                         <img src="./img/project/DMCSite.png" alt="DMCSite"/>
-                        <span><a href="">DMC Site</a></span>
+                        <span><a href="#"><button type="button" onclick="modalpopup(4)">DMC Site</button></a></span>
                     </div>
                     <div className={styles.project}>
                         <img src="./img/project/DMCAPP.png" alt="DMCAPP"/>
-                        <span><a href="">DMC APP</a></span>
+                        <span><a href="#"><button type="button" onclick="modalpopup(5)">DMC APP</button></a></span>
                     </div>
                     <div className={styles.project}>
                         <img src="./img/project/dadok.png" alt="DadokDadok"/>
-                        <span><a href="">Dadok Dadok</a></span>
+                        <span><a href="#"><button type="button" onclick="modalpopup(6)">Dadok Dadok</button></a></span>
                     </div>
                 </div>
             </section>
