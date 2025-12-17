c22fc846911a039fc6acb58cae2aeafe61f04858
wogur2689 <anfvnxntm@naver.com>
2025-12-17 11:00:34 +0900

    fix : 현업 프로젝트 추가

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -4,16 +4,38 @@ import React, {useState} from 'react';
 
 //project data 추후 백엔드로 변경
 const srcArr = [
+    './img/project/HappySum.png',
+    './img/project/Hyundai.png',
     './img/project/Portfolio.png',
 ];
 
 const altArr = [
+    'HappySum',
+    'Hyundai',
     'portfolio',
 ];
 
 const data = [ 
         {
             'id' : 1,
+            'title' : '행복더하기',
+            'outline' : '지자체 복지플랫폼',
+            'skill' : 'Spring boot, JPA, QueryDsl, Vue',
+            'period': '2025.02 ~ 2025.12 (10개월)',
+            'cont1': '관리자 페이지 고도화',
+            'cont2': '',
+        },
+        {
+            'id' : 2,
+            'title' : '현대오일뱅크 AWS 클라우드 이관',
+            'outline' : '노후된 시스템 마이그레이션 및 물리서버에서 클라우드 전환',
+            'skill' : 'AWS, Linux, FTP, Jenkins, Docker, Nexus, Tomcat',
+            'period': '2023.05 ~ 2024.10 (1년 5개월)',
+            'cont1': 'Build 방식 변경 : Ant → Maven',
+            'cont2': 'Jenkins CI/Cd 구축',
+        },
+        {
+            'id' : 3,
             'title' : 'portfolio',
             'outline' : 'next.js14를 이용하여 나의 대표적인 성과를 소개하는 페이지.',
             'skill' : 'Next.js, Pigma',
@@ -82,11 +104,14 @@ const Alert = ({id, open, close}) => {
                     <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
                         <button className={styles.close} onClick={close}>&times;</button>
                         <h1>
+                            {projectData.url ? (
                             <a href={projectData.url} target="_blank" rel="noopener noreferrer">
                                 {projectData.title}
                             </a>
+                            ) : projectData.title
+                            }
                         </h1>
-                        <span>프로젝트 제목을 클릭 시 사이트로 이동합니다.</span>
+                        <span>기간 : {projectData.period}</span>
                         <div>
                             <h3>
                                 <span className={styles.sectionTitle}>개요</span>
@@ -97,15 +122,18 @@ const Alert = ({id, open, close}) => {
                                 <br/>{projectData.skill}
                             </h3>
                             <h3>
-                                <span className={styles.sectionTitle}>기간</span>
-                                <br/>{projectData.period}
-                            </h3>
-                            <h3>
                                 <span className={styles.sectionTitle}>기여</span>
                                 <ul>
-                                    <li>{projectData.cont1}</li>
-                                    <li>{projectData.cont2}</li>
+                                    {projectData.cont1 ? ( <li> {projectData.cont1}  </li>) : ''}
+                                    {projectData.cont2 ? ( <li> {projectData.cont2}  </li>) : ''}
+                                    {projectData.cont3 ? ( <li> {projectData.cont3}  </li>) : ''}
+                                    {projectData.cont4 ? ( <li> {projectData.cont4}  </li>) : ''}
+                                    {projectData.cont5 ? ( <li> {projectData.cont5}  </li>) : ''}
                                 </ul>
+                            </h3>
+                            <h3>
+                                <span className={styles.sectionTitle}>성과</span>
+                                <br/>{projectData.period}
                             </h3>
                         </div>
                     </div>
